a5899f7f8d4edb205b481bfec0c37872683629fa
Patrick McQuay <patrick.mcquay@gmail.com>
2025-03-29 17:17:10 -0400

    Add .gitignore

---
+++ src/decision_maker/base/state.py
@@ -0,0 +1,43 @@
+from abc import ABC, abstractmethod
+from .action import BaseAction
+from collections.abc import Iterable
+
+class BaseState(ABC):
+    """
+    Base class for game state 
+    """
+
+    @abstractmethod
+    def get_current_player(self) -> int:
+        """
+        Returns id for player whose turn it is
+        """
+        raise NotImplementedError()
+    
+    @abstractmethod
+    def get_actions(self) -> Iterable[BaseAction]:
+        """
+        Return a list of all actions (possible or not) for this state
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def get_valid_actions(self) -> Iterable[BaseAction]:
+        """
+        Return a list of all valid actions from this state
+        """
+        raise NotImplementedError()
+    
+    @abstractmethod
+    def take_action(self, action: BaseAction) -> 'BaseState':
+        """
+        Return the state that follows this one after taking a specific action
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def get_reward(self, player_id: int) -> float:
+        """
+        Return the reward for a given player id in the current state
+        """
+        raise NotImplementedError()
